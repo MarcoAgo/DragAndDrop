f63cc5d76911b0760e377fc7ef4dd14ed514ee1d
marco.agostinelli@milkman.it <marco.agostinelli1996@gmail.com>
2020-03-16 19:42:56 +0100

    Updates

---
+++ precache-manifest.cce85b385562ec4de2e6825868cb76b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f73b4b4717bab7237fa25819741d9a",
+    "url": "/DragAndDrop/index.html"
+  },
+  {
+    "revision": "632eacb968a8c38e9c2c",
+    "url": "/DragAndDrop/static/css/main.52007b2f.chunk.css"
+  },
+  {
+    "revision": "124b8f46df2e3f8ff777",
+    "url": "/DragAndDrop/static/js/2.8b61eb49.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/DragAndDrop/static/js/2.8b61eb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632eacb968a8c38e9c2c",
+    "url": "/DragAndDrop/static/js/main.c6262ed7.chunk.js"
+  },
+  {
+    "revision": "b8a5f34f83b3cb0e159a",
+    "url": "/DragAndDrop/static/js/runtime-main.ec62f678.js"
+  }
+]);
